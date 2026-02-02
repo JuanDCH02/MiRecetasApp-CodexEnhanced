3b79edd7f799ed0a93c0d19065be2f820c3045e6
Juan David <juandclementeh@gmail.com>
2026-02-02 17:05:47 -0300

    identando bien el form login

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -6,7 +6,7 @@ export const connectDB= async ()=> {
     try {
         const {connection} = await mongoose.connect(process.env.DATABASE_URL)
         const url = `${connection.host} : ${connection.port}`
-
+        console.log(url)
     } catch (error) {
         console.log(error)
         exit(1)
--- frontend/src/pages/auth/LoginView.tsx
+++ frontend/src/pages/auth/LoginView.tsx
@@ -10,87 +10,86 @@ export default function LoginView() {
 
    const navigate = useNavigate()
    const { register, handleSubmit,reset, formState: { errors } } = useForm<UserLoginForm>()
-   const {mutate} = useMutation({
-       mutationFn: authenticate,
-       onError:(error)=> {
-           toast.error(error.message)
-       },
-       onSuccess:()=> {
-           toast.success('Iniciando sesión')
-           reset()
-           navigate('/')
-       }
-   })
-   const handleLogin = (formData: UserLoginForm) => mutate(formData)
+   const { mutate } = useMutation({
+        mutationFn: authenticate,
+        onError: (error) => { toast.error(error.message) },
+        onSuccess: () => {
+        toast.success('Iniciando sesión')
+        reset()
+        navigate('/')
+    }
+    })
+    const handleLogin = (formData: UserLoginForm) => mutate(formData)
+   
 
-  return (
+    return (
     <>
       
-         <p className="text-2xl font-light text-white mt-5">
+        <p className="text-2xl font-light text-white mt-5">
             Llena el formulario para
             <span className=" text-teal-300 font-bold text-shadow-lg/20"> iniciar sesión</span>
         </p>
-      <form
-        onSubmit={handleSubmit(handleLogin)}
-        className="space-y-5 p-10 bg-white mt-5"
-        noValidate
-      >
+        <form
+            onSubmit={handleSubmit(handleLogin)}
+            className="space-y-5 p-10 bg-white mt-5"
+            noValidate
+        >
+        
         <div className="flex flex-col gap-5">
-          <label
-            className="text-2xl"
-            >Email
-        </label>
+            <label
+                className="text-2xl"
+                >Email
+            </label>
 
-          <input
-            id="email"
-            type="email"
-            placeholder="Email de Registro"
-            className="w-full p-3  border-gray-300 border"
-            {...register("email", {
-              required: "El Email es obligatorio",
-              pattern: {
-                value: /\S+@\S+\.\S+/,
-                message: "E-mail no válido",
-              },
-            })}
-          />
-          {errors.email && (
-            <ErrorMessage>{errors.email.message}</ErrorMessage>
-          )}
+            <input
+              id="email"
+              type="email"
+              placeholder="Email de Registro"
+              className="w-full p-3  border-gray-300 border"
+              {...register("email", {
+                required: "El Email es obligatorio",
+                pattern: {
+                  value: /\S+@\S+\.\S+/,
+                  message: "E-mail no válido",
+                },
+              })}
+            />
+            {errors.email && (
+              <ErrorMessage>{errors.email.message}</ErrorMessage>
+            )}
         </div>
 
         <div className="flex flex-col gap-5">
-          <label
-            className="text-2xl"
-            >Contraseña
+            <label className="text-2xl"
+              >Contraseña
         </label>
 
-          <input
-            type="password"
-            placeholder="Password de Registro"
-            className="w-full p-3  border-gray-300 border"
-            {...register("password", {
-              required: "La contraseña es obligatoria",
-            })}
-          />
-          {errors.password && (
-            <ErrorMessage>{errors.password.message}</ErrorMessage>
-          )}
+            <input
+                type="password"
+                placeholder="Password de Registro"
+                className="w-full p-3  border-gray-300 border"
+                {...register("password", {
+                  required: "La contraseña es obligatoria",
+                })}
+            />
+            {errors.password && (
+                <ErrorMessage>{errors.password.message}</ErrorMessage>
+            )}
         </div>
 
         <input
-          type="submit"
-          value='Iniciar Sesión'
-          className="bg-indigo-500 hover:bg-indigo-800 w-full p-3  text-white font-black  text-xl cursor-pointer"
+            type="submit"
+            value='Iniciar Sesión'
+            className="bg-indigo-500 hover:bg-indigo-800 w-full p-3  text-white font-black  text-xl cursor-pointer"
         />
-      </form>
+        </form>
 
-      <nav className="mt-10 flex flex-col space-y-3">
-            <Link to={`/auth/create-account`}
-            className="text-center font-light text-white"
-                >¿No tienes una cuenta? Registrate aquí
-            </Link>
-      </nav> 
+        <nav className="mt-10 flex flex-col space-y-3">
+              <Link to={`/auth/create-account`}
+              className="text-center font-light text-white"
+                  >¿No tienes una cuenta? Registrate aquí
+              </Link>
+        </nav> 
     </>
-  )
+    )
 }
